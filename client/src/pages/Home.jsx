214e79757571f3d8978f7d240c60e384416dc8e4
myroslavkosyk81 <myroslav.kosyk@ukr.net>
2024-11-20 02:07:47 +0200

    c

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -163,8 +163,6 @@ export default function Home() {
         ))
       }
       </Swiper>
-      
-     
 
       {/* listing resuits */}
       <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10'>
